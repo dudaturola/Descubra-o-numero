c4300baa99c7ddca56aed09f21eab2320aa932bb
Maria Eduarda <idturola@gmail.com>
2024-07-08 11:30:06 -0300

    adicionei o comando while
    Descobri que quando quiser que o algoritimo continue repetindo algo ate que seja verdadeiro um valor é so utilizar o while.

--- app.js
+++ app.js
@@ -1,11 +1,19 @@
 alert('Boas vindas ao jogo do Número Secreto!');
-let numeroSecreto = 4;
-let chute = prompt('Escolha um número entre 1 e 10');
+let numeroSecreto = 5;
+let chute
 console.log('Valor do chute:', chute);
 console.log('Resultado da comparacao', chute == numeroSecreto);
-if (chute == numeroSecreto) {
-    alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto}` );
-} else {
-    alert ('Voce errou! tente novamente');
-    console.log('Valor do numero secreto:', numeroSecreto);
-};
+// enquanto o chute nao for igual ao numero secreto
+while (chute != numeroSecreto) {
+    chute = prompt('Escolha um número entre 1 e 10');
+    if (chute == numeroSecreto) {
+        alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto}` );
+    } else {
+        if(chute > numeroSecreto) {
+            alert(`O número secreto é menor que ${chute}`)
+        } else {
+            alert(`O número secreto é maior que ${chute}`)
+        }
+    }
+}
+
