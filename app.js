19b33e1c0482337c85f68df7a518625ec1b85ccf
Maria Eduarda <idturola@gmail.com>
2024-07-08 11:36:45 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,19 +1,23 @@
 alert('Boas vindas ao jogo do Número Secreto!');
 let numeroSecreto = 5;
-let chute
+let chute;
+let tentativas = 1;
+
 console.log('Valor do chute:', chute);
 console.log('Resultado da comparacao', chute == numeroSecreto);
 // enquanto o chute nao for igual ao numero secreto
 while (chute != numeroSecreto) {
     chute = prompt('Escolha um número entre 1 e 10');
     if (chute == numeroSecreto) {
-        alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto}` );
+        alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativas.` );
     } else {
         if(chute > numeroSecreto) {
-            alert(`O número secreto é menor que ${chute}`)
+            alert(`O número secreto é menor que ${chute}`);
         } else {
-            alert(`O número secreto é maior que ${chute}`)
+            alert(`O número secreto é maior que ${chute}`);
         }
+        //tentativas = tentativas + 1;
+        tentativas++;
     }
 }
 
