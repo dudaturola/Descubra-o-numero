15a49c7eaa08352e0f0b9441aa9ddb8903b5442d
Maria Eduarda <idturola@gmail.com>
2024-07-08 09:53:51 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
-alert('Boas vindas ao jogo do Número Secreto!');
-let numeroSecreto = 5;
-let chute = prompt('Escolha um número entre 1 e 10');
 
-if (chute == numeroSecreto) {
-    console.log('Isso ai! Você descobriu o número secreto (5)');
+nome = prompt('Qual seu nome?');
+idade = prompt('Digite sua idade');
+
+if (idade >= 18) {
+    alert ('Pode tirar habilitacao!')
 }
+else {
+    alert ('Nao pode tirar habilitacao!')
+};
