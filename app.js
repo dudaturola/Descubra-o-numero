d6667aae5a2680453e2030bdeafc22d092917a42
Maria Eduarda <idturola@gmail.com>
2024-07-08 10:42:46 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 alert('Boas vindas ao jogo do Número Secreto!');
 let numeroSecreto = 4;
-console.log(numeroSecreto);
 let chute = prompt('Escolha um número entre 1 e 10');
-//
+console.log('Valor do chute:', chute);
+console.log('Resultado da comparacao', chute == numeroSecreto);
 if (chute == numeroSecreto) {
     alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto}` );
 } else {
     alert ('Voce errou! tente novamente');
-}
+    console.log('Valor do numero secreto:', numeroSecreto);
+};
