ae7be76e78b695d515e6c82fb356b29937c7d9f8
Maria Eduarda <idturola@gmail.com>
2024-07-09 18:47:33 -0300

    Update app.js

--- app.js
+++ app.js
@@ -6,10 +6,13 @@ let tentativas = 1;
 console.log('Valor do chute:', chute);
 console.log('Resultado da comparacao', chute == numeroSecreto);
 // enquanto o chute nao for igual ao numero secreto
+
+
 while (chute != numeroSecreto) {
     chute = prompt('Escolha um número entre 1 e 10');
     if (chute == numeroSecreto) {
-        alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativas.` );
+        break;
+        
     } else {
         if(chute > numeroSecreto) {
             alert(`O número secreto é menor que ${chute}`);
@@ -21,3 +24,12 @@ while (chute != numeroSecreto) {
     }
 }
 
+let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa';
+alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} ${palavraTentativa}.`);
+
+/*
+if (tentativas > 1) {
+    alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativas.` );
+}else{
+    alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativa.` );
+}*/
